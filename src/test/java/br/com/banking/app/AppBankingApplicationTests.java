03df75ac20d365ca38ac7421756dfc065bf828a9
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-04-19 23:43:12 -0300

    "First" commit

---
+++ src/test/java/br/com/banking/app/AppBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.banking.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
