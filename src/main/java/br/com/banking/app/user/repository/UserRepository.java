5d0aabec8ebb0da1c4f4114bfdd513183c955f02
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-04-21 21:05:38 -0300

    Fixing typos

--- src/main/java/br/com/banking/app/user/repository/UserRepository.java
+++ src/main/java/br/com/banking/app/user/repository/UserRepository.java
@@ -3,11 +3,14 @@ package br.com.banking.app.user.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.banking.app.user.model.User;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByEmail(String email);
+
 
 }
