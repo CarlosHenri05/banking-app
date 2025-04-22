293142db9dfb22870c8e8dd1d9c18e918767eb4a
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-04-21 21:05:15 -0300

    Adding @Builder to User model

--- src/main/java/br/com/banking/app/user/model/User.java
+++ src/main/java/br/com/banking/app/user/model/User.java
@@ -1,5 +1,6 @@
 package br.com.banking.app.user.model;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -7,23 +8,39 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 public class User implements UserDetails{
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(unique = true)
   private String email;
+
   private String password;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
 
   @Enumerated(EnumType.STRING)
   private Roles role;
@@ -57,5 +74,7 @@ public class User implements UserDetails{
   public boolean isEnabled() {
     return true;
   }
+
+  
 
 }
