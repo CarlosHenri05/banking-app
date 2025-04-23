5fd515b1b38b0307fb5668c8067c964f9c286b43
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-04-23 11:19:22 -0300

    Adding POST LogIn and Register endpoints

--- src/main/java/br/com/banking/app/auth/controller/AuthController.java
+++ src/main/java/br/com/banking/app/auth/controller/AuthController.java
@@ -1,14 +1,36 @@
 package br.com.banking.app.auth.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.banking.app.auth.dto.AuthResponseDTO;
+import br.com.banking.app.auth.dto.LoginRequestDTO;
+import br.com.banking.app.auth.dto.RegisterRequestDTO;
+import br.com.banking.app.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
+  final AuthService authService;
+
+
+
+
+  @PostMapping("/auth/login")
+  public ResponseEntity<AuthResponseDTO> userLogin (@RequestBody LoginRequestDTO loginRequestDTO) {
+    return ResponseEntity.ok(authService.signIn(loginRequestDTO));
+  }
+
+  @PostMapping("/auth/register") 
+  public ResponseEntity<AuthResponseDTO> userRegister (@RequestBody RegisterRequestDTO RegisterRequestDTO) {
+    return ResponseEntity.ok(authService.signUp(RegisterRequestDTO));
+  }
 
   
 
