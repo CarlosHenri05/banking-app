41003560fdacb269e0fc4342ec56f14addabc8d9
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-05-04 16:37:48 -0300

    Modifying DTO's for better request and response

--- src/main/java/br/com/banking/app/transaction/dto/TransactionResponseDTO.java
+++ src/main/java/br/com/banking/app/transaction/dto/TransactionResponseDTO.java
@@ -2,27 +2,28 @@ package br.com.banking.app.transaction.dto;
 
 import java.time.OffsetDateTime;
 
-import br.com.banking.app.user.model.User;
-import io.micrometer.common.lang.NonNull;
+
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class TransactionResponseDTO {
-  @NonNull
+
+  private long id;
+
   @Positive
   private double amount;
 
   @NotNull
-  private User user;
+  private String username;
 
-  @NotNull 
+  @NotNull
   private OffsetDateTime time;
 }
