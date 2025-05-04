b8bfe1055ac38875140e652e38c9268afdc0ac4a
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-05-04 16:41:23 -0300

    Adding scanBasePackages because transaction was not being found

--- src/main/java/br/com/banking/app/AppBankingApplication.java
+++ src/main/java/br/com/banking/app/AppBankingApplication.java
@@ -3,7 +3,7 @@ package br.com.banking.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"br.com.banking.app", "br.com.banking.app.transaction"})
 public class AppBankingApplication {
 
 	public static void main(String[] args) {
