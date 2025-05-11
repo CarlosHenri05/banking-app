5821a4c84c11a3173fa25ecdf0fec593d3f4987d
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-05-10 21:01:46 -0300

    Adding methods to return transactions from same category and income

--- src/main/java/br/com/banking/app/transactions/service/TransactionService.java
+++ src/main/java/br/com/banking/app/transactions/service/TransactionService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import br.com.banking.app.transactions.model.Transaction;
+import br.com.banking.app.transactions.model.TransactionCategory;
+import br.com.banking.app.transactions.model.TransactionType;
 import br.com.banking.app.transactions.repository.TransactionRepository;
 import br.com.banking.app.user.model.User;
 import br.com.banking.app.user.repository.UserRepository;
@@ -18,7 +20,8 @@ public class TransactionService {
     private final TransactionRepository transactionRepository;
 
   public void saveTransaction(Transaction transaction, long id){
-    User user = userRepository.findById(id).orElseThrow( () -> new RuntimeException("User was not found"));
+      User user = userRepository.findById(id)
+                                .orElseThrow(() -> new IllegalArgumentException("User not found"));
 
     transaction.setUser(user);
     transactionRepository.save(transaction); 
@@ -34,10 +37,30 @@ public class TransactionService {
   }
 
   public List<Transaction> getAllTransactionsFromUser(long userId) {
-    User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Transaction not found"));
+      User user = userRepository.findById(userId)
+          .orElseThrow(() -> new IllegalArgumentException("User not found"));
 
-    return user.getTransactions().stream()
-                                 .toList();
+
+      return user.getTransactions().stream()
+                                  .toList();
+  }
+
+  public List<Transaction> getAllTransactionsFromSameCategory(long userId, TransactionCategory category){
+      User user = userRepository.findById(userId)
+          .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+      return user.getTransactions().stream()
+                                  .filter(transaction -> transaction.getCategory() == category)
+                                  .toList();
   }
   
+  public List<Transaction> getTotalIncome(long userId){
+    User user = userRepository.findById(userId)
+                              .orElseThrow(() -> new IllegalArgumentException("User not found"));
+    
+    return user.getTransactions().stream()
+                                 .filter(transaction -> transaction.getType() == TransactionType.INCOME)
+                                 .toList();
+  }
+
 }
